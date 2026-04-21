c78fa54c41b551238157ed8b224b0210b54c09f4
Andreas Myhre Johnsrud <anjoh7871@uit.no>
2026-04-21 21:14:05 +0200

    lagt til repository og viewmodel for darkmode

--- app/src/main/java/com/example/followme02/FollowMeApp.kt
+++ app/src/main/java/com/example/followme02/FollowMeApp.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -40,7 +41,9 @@ import com.example.followme02.viewmodel.WorkoutViewModel
 @Composable
 fun FollowMeApp(
     isDarkMode: Boolean,
-    onToggleDarkMode: () -> Unit
+    onToggleDarkMode: () -> Unit,
+    onLoadDarkMode: () -> Unit,
+    onResetDarkMode: () -> Unit
 ) {
     val navController = rememberNavController()
 
@@ -52,6 +55,14 @@ fun FollowMeApp(
 
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
+
+    LaunchedEffect(currentRoute) {
+        when (currentRoute) {
+            "login", "register" -> onResetDarkMode()
+            null -> Unit
+            else -> onLoadDarkMode()
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -71,9 +82,7 @@ fun FollowMeApp(
             navController = navController,
             startDestination = "login",
             modifier = Modifier.padding(padding)
-        )
-        {
-
+        ) {
             composable("login") {
                 LoginScreen(navController, authViewModel)
             }
